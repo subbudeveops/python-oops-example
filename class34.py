93def6c0d2b866d1448e639307b848a82e7deb14
subbareddy <subbareddyreddam@gmail.com>
2021-10-09 16:19:45 +0530

    first commit

--- class34.py
+++ class34.py
@@ -1,1 +1,83 @@
-#Regular Expression
+#Abstract class and Abstract methods
+from abc import ABC,abstractmethod
+class Person(ABC):
+    @abstractmethod
+    def display(self):
+        pass
+class Employee(Person):
+    def display(self):
+        print('i have one idea')
+class Subbu(Person):
+    def display(self):
+        print('i have two idea')
+class Bhavani(Person):
+    def display(self):
+        print('i have three idea')
+e=Employee()
+s=Subbu()
+b=Bhavani()
+e.display()
+s.display()
+b.display()
+
+
+print()
+class Parent(ABC):
+    def m1(self):
+        pass
+    def m2(self):
+        pass
+class Child1(Parent):
+    def m1(self):
+        print('child1 class method')
+class Child2(Child1):
+    def m2(self):
+        print('chilsd2 class method')
+
+c2=Child2()
+c2.m1()
+c2.m2()
+
+print()
+import abc
+class P(ABC):
+    def s(self):
+        print('Abstract Base Class')
+class C(P):
+    def s(self):
+        super().s()
+        print('sub class')
+c=C()
+c.s() 
+
+#example
+class A(ABC):
+    def __init__(self,value):
+        self.value=value
+    
+    @abstractmethod
+    def disp1(self):
+        pass
+class B(A):
+    def disp1(self):
+        print(0+self.value)
+
+class mul(A):
+    def disp1(self):
+        print(0*self.value)
+a=B(10)        
+m=mul(5)
+a.disp1()
+m.disp1()
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ class35.py
@@ -0,0 +1,51 @@
+#public protect and private members 
+# how to define public and how to acess public variable
+#public variable acess any were
+# All the class variable are public
+class Car:
+    def __init__(self,windows,doors,enginetype):
+        self.windows=windows
+        self.doors=doors
+        self.engintype=enginetype
+c=Car(4,5,'audi')
+
+c.windows=5#here we can over write  values as changes
+print(c.windows)#5
+
+
+
+
+#All the class variable are protected
+# if you want change proctect variable we cant change in parent class only we can change the child class only
+#protect variable acess only subclass or child class
+
+class A:
+    def __init__(self,name,age,sal):
+        self._name=name
+        self._age=age
+        self._sal=sal
+class B(A):
+    def __init__(self,name,age,sal,add):
+        super().__init__(name,age,sal)
+        self.add=add
+
+a=B('AUDI',30,1000,'sarvayapalli')
+print(a._name)
+a._name='subbu'#override 
+print(a._name)
+
+
+print()
+#private vriable
+#private variable we cannot override and wea cannot  acess any where 
+# private variable cannot aceed and  cannot modified out side of the class
+# Hoe to define
+class A:
+    def __init__(self,name,age,sal):
+        self.__name=name
+        self.__age=age
+        self.__sal=sal
+a=A('subbu',10,1000)  
+print(a._A__name)
+a._A__name='ravi'
+print(a._A__name)
